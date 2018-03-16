65c8a9698666e67a3e8528d9d9f28545d9fc3757
Fanny Gaudin <mauriceleptitpoissonrouge@hotmail.fr>
2018-03-16 09:19:13 +0100

    ajout de la partie de Gabriel dans index.html et mise à jour du fichier css

--- js/javascript.js
+++ js/javascript.js
@@ -89,4 +89,47 @@ $(document).ready(function(){
             updateGallery($(this));
         });
     }
+
+    // Add smooth scrolling to all links
+      $(".navbar .navbar-nav > a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+
+    // // Get the modal
+    // var modal = $('#myModal');
+
+    // // Get the image and insert it inside the modal - 
+    // var img = $('.modalG');
+    // var modalImg = $(".modal-content");
+
+    // img.click(function(){
+    //     modal.css('display','block');
+    //     modalImg.attr('src') = $(this).attr('src');
+    // });
+
+    // // Get the <span> element that closes the modal
+    // var span = $(".close");
+
+    // // When the user clicks on <span> (x), close the modal
+    // span.click(function() { 
+    //     modal.css('display','none');
+    // });
 });
